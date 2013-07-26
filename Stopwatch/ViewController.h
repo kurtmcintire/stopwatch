2b2943dfb86152f286a605174df2c15be9164bcc
Kurt McIntire <kurt.mcintire@gmail.com>
2013-07-26 18:03:15 -0500

    Added a stopwatch code to ViewController. Still missing handling to reset or continue stopwatch time.

--- Stopwatch/ViewController.h
+++ Stopwatch/ViewController.h
@@ -9,5 +9,19 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    BOOL boolActive;
+    int intTime;
+}
+
+@property (nonatomic, strong) NSTimer *timerMain;
+@property (weak, nonatomic) IBOutlet UILabel *timerLabel;
+@property (weak, nonatomic) IBOutlet UIButton *startStopButton;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+
+
+
+- (IBAction)timerAction:(id)sender;
+-(void)timerControl;
 
 @end
--- Stopwatch/ViewController.m
+++ Stopwatch/ViewController.m
@@ -20,12 +20,56 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Whenever the app loads, the stopwatch is not active.
+    boolActive = NO;
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)timerAction:(id)sender {
+    
+    // Check if stopwatch active
+    
+    if (boolActive == NO) {
+       
+        // If not active
+        _timerMain = [NSTimer scheduledTimerWithTimeInterval:1.0/1.0 target:self selector:@selector(timerControl) userInfo:nil repeats:YES];
+        
+        // Set the bool to Active now that timerControl is called
+        boolActive = YES;
+        
+        // Now that the stopwatch is running, change the button title to Stop
+        [_startStopButton setTitle:@"Stop" forState:UIControlStateNormal];
+        
+        intTime = 0;
+        NSLog(@"Stopwatch started");
+        
+    } else {
+        
+        // If active
+        [_timerMain invalidate];
+        
+        boolActive = NO;
+        
+        [_startStopButton setTitle:@"Start" forState:UIControlStateNormal];
+        
+        NSLog(@"Stopped");
+        
+    }
+}
+
+-(void)timerControl {
+    
+    intTime += 1;
+    
+    _timerLabel.text = [NSString stringWithFormat:@"%i", intTime];
+    NSLog(@"Stopwatch time = %i", intTime);
+    
 }
 
 @end
