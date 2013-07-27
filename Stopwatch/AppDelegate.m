8fe4bd496af82959d0a644a8b5931814bc16e6de
Kurt McIntire <kurt.mcintire@gmail.com>
2013-07-27 11:15:51 -0500

    Now I’m attaching the NSDate to a variable called backgroundDate when the application goes into the background.

--- Stopwatch/AppDelegate.m
+++ Stopwatch/AppDelegate.m
@@ -29,11 +29,15 @@
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     
-    NSString *dateString = [NSDateFormatter localizedStringFromDate:[NSDate date]
-                                                          dateStyle:NSDateFormatterShortStyle
-                                                          timeStyle:NSDateFormatterFullStyle];
-    NSLog(@"Entered Background");
+    _backgroundDate = [NSDate date];
+    
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"MM/dd/yyyy, HH:mm:ss"];
+    NSString *dateString = [dateFormatter stringFromDate:_backgroundDate];
+    
     NSLog(@"%@", dateString);
+    NSLog(@"%@", _backgroundDate);
+    NSLog(@"Entered Background");
     
     
 }
